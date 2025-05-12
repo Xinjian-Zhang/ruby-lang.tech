74473a5449ded576f613b8572681c4fc44e53aa9
Xinjian-Zhang <1xinjian.zhang@gmail.com>
2025-05-12 12:43:55 +0300

    log: update 02 on 2025-05-12

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747041878',resources: [
+    cacheName: 'chirpy-1747042895',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
